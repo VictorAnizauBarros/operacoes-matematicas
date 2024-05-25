fcf2d2d266be927d3e9b5c60e71d320e1a37978e
VictorAnizauBarros <victorhugo.AnizauEstudos@outlook.com>
2024-05-24 23:55:40 -0300

    Add files via upload

---
+++ operacoes.sh
@@ -0,0 +1,14 @@
+
+read -p "Digite o primeiro número: " num1
+read -p "Digite o segundo número: " num2
+
+
+echo "Adição: $(echo "$num1 + $num2" | bc)"
+echo "Subtração: $(echo "$num1 - $num2" | bc)"
+echo "Multiplicação: $(echo "$num1 * $num2" | bc)"
+
+if [ "$num2" == "0" ]; then
+    echo "Divisão: Erro: Divisão por zero"
+else
+    echo "Divisão: $(echo "scale=2; $num1 / $num2" | bc)"
+fi
